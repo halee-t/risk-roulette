014ff76ebe3b62e1b40b36f5f2292f8c253e42c8
halee-t <haleetisler@gmail.com>
2025-02-02 04:00:16 -0500

    okay this branch isn't broken :)

--- src/components/Fire.tsx
+++ src/components/Fire.tsx
@@ -1,9 +1,50 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Answer from "./answer";
+import answerData from "../data/fire-questions.json";
 
-const Fire = () => {
+interface QuestionProps {
+  question: string;
+  answers: string[];
+  correct: boolean[];
+}
+
+const Fire: React.FC = () => {
+  const [currentQuestion, setCurrentQuestion] = useState<QuestionProps | null>(
+    null
+  );
+
+  useEffect(() => {
+    // Pick a random question from the answerData
+    const randomQuestionIndex = Math.floor(
+      Math.random() * answerData.questions.length
+    ); // Random question from the selected tile
+    const selectedQuestion = answerData.questions[randomQuestionIndex];
+
+    setCurrentQuestion(selectedQuestion);
+  }, []);
+
+  if (!currentQuestion) {
+    return <div>Loading...</div>;
+  }
   return (
-    <div>
-      <h1>placeholder</h1>
+    <div className="font-semibold font-sans text-center flex flex-col items-center">
+      <div className="">
+        <h1>insert scenario still</h1>
+        <h1>insert pic still</h1>
+      </div>
+      <section className="px-12 flex flex-col items-center justify-center ring ring-2 ring-red gap-8">
+        <div className="underline">{currentQuestion.question}</div>
+        <div className="h-48 w-full grid grid-rows-2 grid-cols-2 ">
+          {currentQuestion.answers.map((answer, index) => (
+            <div className="flex justify-center" key={index}>
+              <Answer
+                answerStr={answer}
+                correctness={currentQuestion.correct[index]}
+              ></Answer>
+            </div>
+          ))}
+        </div>
+      </section>
     </div>
   );
 };
