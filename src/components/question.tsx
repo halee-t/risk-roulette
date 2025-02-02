c3d07f232005aa73374903253bc26816f7d5263f
Sean Collins <yubtubfromtheyubtub@gmail.com>
2025-02-01 19:15:09 -0500

    added timer to the question component that counts from 10 to 0

--- src/components/question.tsx
+++ src/components/question.tsx
@@ -10,10 +10,27 @@ interface questionProps{
 
 
 const Question: React.FC<questionProps> = (props) => {
+    const [timer, setTimer] = useState(10)
+
+useEffect(() => {
+    const interval = setInterval(() => {
+        setTimer(prevTimer => {
+            if(prevTimer>0){
+                return prevTimer - 1
+            }else{
+                return 0
+            }
+        })
+    },1000)
+    return () => clearInterval(interval);
+}, [timer])
+    
     return (<>
     <div className="font-semibold font-sans text-center underline">
         {props.question}
     </div>
+    <div className="text-center">Time remaining:</div>
+    <div className="text-center">{timer}</div>
     <div className="grid grid-cols-2 w-1/2 mt-1 gap-1 text-justify">
         {props.answers.map((res, index) => (
             <div>
